79aa1e9d926930e4953fd88058a378cc2489ba53
Daniel Li <dli@nextdoor.com>
2018-10-03 20:31:50 -0700

    status save

---
+++ reflex-app/src/animations/TabSwitch/TabSwitch.js
@@ -0,0 +1,16 @@
+import React, { Fragment, Component, PureComponent } from 'react';
+import PropTypes from 'prop-types';
+import styled, { keyframes } from 'styled-components';
+
+const TabSwitch = styled.div`
+  display: grid;
+  grid-template-rows: auto auto;
+`;
+
+
+// how do you apply the onClick to the dynamic chilren?
+// How do you apply it such that it doesn't override the onClick
+// Maybe it can't have an onClick for now
+TabSwitch.
+
+export default TabSwitch;
