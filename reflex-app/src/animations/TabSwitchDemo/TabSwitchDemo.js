98f01f2fc73f192bd12a003c357b53772bf40d4a
Daniel Li <dli@nextdoor.com>
2018-12-30 14:58:37 -0800

    Add Tab Switch Animation

--- reflex-app/src/animations/TabSwitchDemo/TabSwitchDemo.js
+++ reflex-app/src/animations/TabSwitchDemo/TabSwitchDemo.js
@@ -1,26 +1,26 @@
 // TODO: Do this component when the detail page is ready. The animation on home page will be a GIF.
-// import React from 'react';
-// import TabSwitch from '../TabSwitch';
-// import styled from "styled-components";
-//
-// const Button = styled.div`
-//   border: none;
-//   outline: none;
-//   padding: 10px 16px;
-// `
-//
-// const test = hover => (
-//   <TabSwitch>
-//     <Button> tst </Button>
-//     <Button> tst </Button>
-//     <Button> tst </Button>
-//     <Button> tst </Button>
-//     <Button> tst </Button>
-//     <Button> tst </Button>
-//   </TabSwitch>
-// )
-//
-// export default {
-//   title: 'Tab Switch',
-//   render: test,
-// }
+import React from 'react';
+import TabSwitch from '../TabSwitch';
+import styled from "styled-components";
+
+const Button = styled.div`
+  border: none;
+  outline: none;
+  padding: 10px 16px;
+`
+
+const test = (
+  <TabSwitch>
+    <Button> This is a test </Button>
+    <Button> This is a test </Button>
+    <Button> This is a test </Button>
+    <Button> This is a test </Button>
+    <Button> This is a test </Button>
+    <Button> This is a test </Button>
+  </TabSwitch>
+)
+
+export default {
+  title: 'Tab Switch',
+  render: test, //TODO: Change this variable name 'test'
+}
