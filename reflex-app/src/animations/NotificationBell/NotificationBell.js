7fe7de66629dbf55ebb35db410caa65736cf65fc
Daniel Li <dli@nextdoor.com>
2019-02-21 19:19:45 -0800

    Fix rebase

---
+++ reflex-app/src/animations/NotificationBell/NotificationBell.js
@@ -0,0 +1,96 @@
+import React, { Fragment, Component, PureComponent } from 'react';
+import PropTypes from 'prop-types';
+import styled, { keyframes } from 'styled-components';
+
+import bellImg from '../../images/bell-solid.svg';
+
+// TODO: This should just specify the size....Why transform?
+// This animation should be on hover for the landing page
+// Should click button to animate?
+
+const notifyShake = keyframes`
+  15.56% {
+    transform: rotate(7deg); }
+  31.12% {
+    transform: rotate(-6deg); }
+  46.68% {
+    transform: rotate(5deg); }
+  62.24% {
+    transform: rotate(-4deg); }
+  77.8% {
+  
+    transform: rotate(3deg); }
+  100% {
+    transform: rotate(0deg); 
+  } 
+`;
+
+const badgeDisappear = keyframes`
+  100% {
+    transform: scale(0);
+  }
+`;
+
+const badgePopIn = keyframes`
+  50% {
+    opacity: 1;
+    transform: scale(1.2); }
+  100% {
+    opacity: 1;
+    transform: scale(1); }
+`;
+
+const fadeIn = keyframes`
+  0% {
+    opacity: 0;
+  }
+  100% {
+    opacity: 1;
+  }
+`;
+
+const Image = styled.img`
+  transform-origin: 50% 0;
+`;
+
+const Badge = styled.div`
+  position: absolute;
+  opacity: 0;
+  top: -1.25px;
+  right: -2px;
+  width: 14px;
+  height: 14px;
+  border-radius: 12px;
+  border: 3px solid #ffffff;
+  background-color: #ee4444;
+`;
+
+const Wrapper = styled.div`
+  position: relative;
+  width: 36px;
+  height: 36px;
+
+  ${Image} {
+    animation: ${props => props.hover && `${notifyShake} ease-out 0.68s`};
+  }
+  
+  ${Badge} { 
+   animation: ${props =>
+     props.hover &&
+     `${badgePopIn} ease-in-out .1s forwards, 
+     ${badgeDisappear} ease-in-out .1s 1s forwards;
+   `}
+`;
+
+const NotificationBell = hover => (
+  <Wrapper hover={hover}>
+    <Image src={bellImg} />
+    <Badge />
+  </Wrapper>
+);
+
+// TODO: Is there a better way to do this?
+export default {
+  title: 'Notification Bell',
+  render: NotificationBell
+};
--- reflex-app/src/animations/TabSwitch/TabSwitch.js
+++ reflex-app/src/animations/TabSwitch/TabSwitch.js
@@ -1,16 +1,77 @@
-import React, { Fragment, Component, PureComponent } from 'react';
-import PropTypes from 'prop-types';
-import styled, { keyframes } from 'styled-components';
+import React, { Component } from 'react';
+import styled from 'styled-components';
+import { Spring } from 'react-spring'
 
-const TabSwitch = styled.div`
-  display: grid;
-  grid-template-rows: auto auto;
-`;
+// TODO: Follow the BEM format of FormInput when creating demos.
+const Container = styled.div`
+	 display: grid;
+   grid-template-rows: auto auto;
+`
 
+const TabContainer = styled.div`
+  display: flex;
+  flex-direction: row;
+`
 
-// how do you apply the onClick to the dynamic chilren?
-// How do you apply it such that it doesn't override the onClick
-// Maybe it can't have an onClick for now
-TabSwitch.
+// TODO: Take in custom styles.
+const Marker = styled.div.attrs({
+  style: ({ left }) => ({ left: left + 'px'})
+})`
+   position: absolute;
+   border-radius: 6px;
+   width: 6px;
+   height: 6px;
+   background-color: #4552E3;
+`
+
+class TabSwitch extends Component {
+  state = {
+    children: [],
+    currentIndex: null,
+    to: 0,
+    from: 0,
+  };
+
+  onSelect = (index) => {
+    const elem = this.state.children[index];
+    const elemBoundingClientRect = elem.getBoundingClientRect();
+    const center = elemBoundingClientRect.left + elemBoundingClientRect.width/2;
+    this.setState({
+      currentIndex: index,
+      from: this.state.to,
+      to: center,
+    });
+  }
+
+// TODO: Should be able to apply the TabSwitch animation even when children are wrapped with container div / Fragment. This is necessary to support the vertical version of this animation.
+  render() {
+    return (
+      <Container>
+       <TabContainer>
+          {React.Children.map(this.props.children, (child, index) =>
+            // Using index is bad. But, it is unlikely the user will use this animation on a list that will be mutated.
+            React.cloneElement(child, {
+              key: index,
+              onClick: () => this.onSelect(index),
+              // TODO: use innerRef for styled components? Shouldn't need to but there issomething weird with versioning
+              ref: (node) => {
+                this.state.children.push(node);
+                // Call the original ref, if any
+                const {ref} = child;
+                if (typeof ref === 'function') {
+                  ref(node);
+                }
+              }
+            })
+          )}
+       </ TabContainer>
+       <Spring from={{ left: this.state.from }} to={{ left: this.state.to }}>
+          {({left}) => <Marker left={left} />}
+        </Spring>
+     </ Container>
+    )
+  }
+}
+
 
 export default TabSwitch;
