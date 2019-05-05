9e3c815a316b1f99a2d82464e6e595c7cd36c122
Jeff Gong <JeffTest@8c859010cb1b.ant.amazon.com>
2019-05-05 09:40:14 -0700

    Update spacing to be more consistent

--- reflex-app/src/animations/LoadingLines/index.js
+++ reflex-app/src/animations/LoadingLines/index.js
@@ -5,43 +5,44 @@ import Line from './line';
 import LineContainer from './line_container';
 
 const AnimationContainer = styled.div`
-	position: relative;
-	width: 100px;
-	height: 100px;
-	margin: 0 auto;
-	transition: all .1s ease-in-out;
+  position: relative;
+  width: 100px;
+  height: 100px;
+  margin: 0 auto;
+  transition: all .1s ease-in-out;
 `
 
 class LoadingLines extends PureComponent {
-	constructor(props) {
-		super(props);
-		this.state = {
-			clicked: false,
-		};
-		this.handleOnClick = this.handleOnClick.bind(this);
-	}
+  constructor(props) {
+    super(props);
+      this.state = {
+        clicked: false,
+      };
+      this.handleOnClick = this.handleOnClick.bind(this);
+  }
 
-	handleOnClick() {
-		this.setState({
-			clicked: !this.state.clicked,
-		});
-	}
+  handleOnClick() {
+    this.setState({
+      clicked: !this.state.clicked,
+    });
+  }
 
-	render() {
-		return (
-			<AnimationContainer onClick={this.handleOnClick}>
-				<LineContainer clicked={this.state.clicked} top="35%" fadeTime={[22, 63, 96]}>
-					<Line clicked={this.state.clicked} scaleSizes={[0.4, 0.56, 0.58]} />
-				</LineContainer>
-				<LineContainer clicked={this.state.clicked} top="45%" left="10%" fadeTime={[26, 62, 95]}>
-					<Line clicked={this.state.clicked} scaleSizes={[0.58, 0.38, 0.40]} />
-				</LineContainer>
-				<LineContainer clicked={this.state.clicked} top="55%" left="5%" fadeTime={[28, 64, 97]}>
-					<Line clicked={this.state.clicked} scaleSizes={[0.38, 0.42, 0.45]} />
-				</LineContainer>
-			</AnimationContainer>
-		);
-	}
+  render() {
+    return (
+      <AnimationContainer onClick={this.handleOnClick}>
+        <LineContainer clicked={this.state.clicked} top="35%" fadeTime={[22, 63, 96]}>
+          <Line clicked={this.state.clicked} scaleSizes={[0.4, 0.56, 0.58]} />
+        </LineContainer>
+        <LineContainer clicked={this.state.clicked} top="45%" left="10%" fadeTime={[26, 62, 95]}>
+          <Line clicked={this.state.clicked} scaleSizes={[0.58, 0.38, 0.40]} />
+        </LineContainer>
+        <LineContainer clicked={this.state.clicked} top="55%" left="5%" fadeTime={[28, 64, 97]}>
+          <Line clicked={this.state.clicked} scaleSizes={[0.38, 0.42, 0.45]} />
+        </LineContainer>
+        </AnimationContainer>
+    );
+  }
 }
 
 export default LoadingLines;
+
