4fcf1ab92a60585f10f1f4a2aaed4c1bd132d3eb
gracesnoh <gracesnoh@gmail.com>
2019-08-11 16:20:26 -0700

    social icons example

--- reflex-app/src/animations/demos/SocialLineIconsDemo/SocialLineIconsDemo.js
+++ reflex-app/src/animations/demos/SocialLineIconsDemo/SocialLineIconsDemo.js
@@ -28,10 +28,57 @@ class SocialLineIconsDemo extends Component {
   }
 }
 
+const ExamplesContainer = styled.div`
+    display: flex;
+    flex-direction: column;
+    justify-content: space-between;
+`;
+
+const Example = styled.div`
+  width: 290px;
+  height: 230px;
+  background-color: #EEEEEE;
+  box-shadow: 0px 2px 5px rgba(0, 0, 0, 0.25);
+  border-radius: 3px;
+  margin-bottom: 18px;
+  display: flex;
+  align-items: flex-end;
+`;
+
+const Footer = styled.div`
+  width: 100%;
+  height: 85%;
+  background-color: #FAFAFA;
+  border-radius: 0 0 3px 3px;
+  padding: 18px 24px;
+  line-height: 2.5;
+`;
+
+const Links = styled.div``;
+
+const Social = styled.div`
+  display: grid;
+  grid-template-columns: repeat(4, 30px);
+  grid-gap: 6px;
+  margin-top: 12px;
+`;
+
 class SocialLineIconsExamples extends Component {
   render() {
     return (
-      <div>Sup</div>
+      <ExamplesContainer>
+        <Example>
+          <Footer>
+            <Links>Careers<br/>About<br/>Locations<br/></Links>
+            <Social>
+              <FBLineIcon/>
+              <TWLineIcon/>
+              <MDLineIcon/>
+              <IGLineIcon/>
+            </Social>
+          </Footer>
+        </Example>
+      </ExamplesContainer>
     );
   }
 }
